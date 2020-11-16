fbc4edabbfa1cdc45650b26124891f6f30960c57
Natalia Venditto <nataliafrontend@gmail.com>
2020-11-16 12:15:14 +0100

    initial commit

---
+++ scully.docs-app.config.ts
@@ -0,0 +1,14 @@
+import { ScullyConfig } from '@scullyio/scully';
+export const config: ScullyConfig = {
+  projectRoot: "./src",
+  projectName: "docs-app",
+  outDir: './dist/static',
+  routes: {
+    '/docs/:slug': {
+      type: 'contentFolder',
+      slug: {
+        folder: "./docs"
+      }
+    },
+  }
+};
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SidenavComponent } from './sidenav/sidenav.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'docs', loadChildren: () => import('./docs/docs.module').then(m => m.DocsModule)
+  },
+  {
+  path: '', redirectTo: 'blog/homepage', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  declarations: [SidenavComponent]
 })
 export class AppRoutingModule { }
